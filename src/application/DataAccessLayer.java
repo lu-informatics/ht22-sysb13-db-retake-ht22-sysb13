9036cd0e65aed5b2051ff699f3bd862cea8b9892
Miguel Soler <93925157+Miguel-Sol@users.noreply.github.com>
2022-11-15 15:28:46 +0000

    Create DataAccessLayer.java
    Created DAL that connects to Marvix Inc. database

---
+++ src/application/DataAccessLayer.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.IOException;
+
+public class DataAccessLayer {
+
+	private String connectionURL;
+
+	public DataAccessLayer() throws IOException {
+
+		try {
+			String databaseServerName = System.getenv("MARVIX_SERVER_NAME");
+			String databaseServerPort = System.getenv("MARVIX_SERVER_PORT");
+			String databaseName = System.getenv("MARVIX_NAME");
+			String databaseUserName = System.getenv("MARVIX_USER_NAME");
+			String databaseUserPassword = System.getenv("MARVIX_USER_PASSWORD");
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			connectionURL = "jdbc:sqlserver://" + databaseServerName + ":" + databaseServerPort + ";" + "database="
+					+ databaseName + ";" + "user=" + databaseUserName + ";" + "password=" + databaseUserPassword + ";"
+					+ "encrypt=true;" + "trustServerCertificate=true";
+		} catch (Exception e) {
+
+		}
+	}
+	
+	
+}
