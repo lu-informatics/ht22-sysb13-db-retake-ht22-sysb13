9ce2c6050946865a5d4aa011ba6c5bf84072bef3
Miguel Soler <93925157+Miguel-Sol@users.noreply.github.com>
2022-11-22 16:45:53 +0000

    Added some DAL methods
    Added following:
    Finding Customer by ID
    Finding Product by ID
    Viewing orders
    finding all orders from a customer

--- src/application/DataAccessLayer.java
+++ src/application/DataAccessLayer.java
@@ -1,10 +1,14 @@
 package application;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DataAccessLayer {
 
-	
 	
 	private String connectionURL;
 
@@ -23,6 +27,107 @@ public class DataAccessLayer {
 		} catch (Exception e) {
 
 		}
+		
+		/* 
+    -Create a Java Connection to the MySQL database
+   - Define the SELECT statement
+   - Execute the SELECT query, getting a Java ResultSet from that query
+   - Iterate over the ResultSet, getting the database fields (columns) from each row of data that is returned
+   - Close the Java database connection
+   - Catch any SQL exceptions that may come up during the process
+
+		 */
+		
+		
+		
 	}
+
+	//Orders
+	//Viewing all orders 
+	
+	public ResultSet viewAllOrders (String orderID) throws SQLException{
+		
+		String query = "SELECT * FROM ORDERS WHERE orderID = ?";
+		Connection connection = DriverManager.getConnection(connectionURL);
+		PreparedStatement pS = connection.prepareStatement(query);
+		pS.setString(1, orderID);
+		pS.executeUpdate();
+		ResultSet rS = pS.executeQuery();
+		
+		rS.close();
+		pS.close();
+		connection.close();
+		
+		return rS;
+	}
+	
+	
+	//Customer
+	//Finding customer by customer ID
+public ResultSet findCustomerByCustomerId( String customerID) throws SQLException{
+		
+		String query = "SELECT * FROM CUSTOMER WHERE customerID = ?";
+		Connection connection = DriverManager.getConnection(connectionURL);
+		PreparedStatement pS = connection.prepareStatement(query);
+		pS.setString(1, customerID);
+		pS.executeUpdate();
+		ResultSet rS = pS.executeQuery();
+		
+		rS.close();
+		pS.close();
+		connection.close();
+		
+		return rS;		
+}
+
+//Finding all orders for a certain customer
+
+public ResultSet findOrdersForCertainCustomer( String customerID) throws SQLException{	
+	
+	String query = "SELECT c.CustomerID, o.OrderID, o.OrderDate"
+			+ " FROM CUSTOMER c "
+			+ "JOIN ORDERS o"
+			+ "ON c.CustomerID = o.CustomerID"
+			+ "WHERE c.CustomerID = ?";
+	Connection connection = DriverManager.getConnection(connectionURL);
+	PreparedStatement pS = connection.prepareStatement(query);
+	pS.setString(1, customerID);
+	pS.executeUpdate();
+	ResultSet rS = pS.executeQuery();
+	
+	rS.close();
+	pS.close();
+	connection.close();
+	
+	return rS;		
+}
+
+
+
+//finding the latest order for a certain customer
+
+
+//Product
+
+//Finding a product by productID
+public ResultSet findProductByProductId( String productID) throws SQLException{
+	
+	String query = "SELECT * FROM PRODUCT WHERE ProductID = ?";
+	Connection connection = DriverManager.getConnection(connectionURL);
+	PreparedStatement pS = connection.prepareStatement(query);
+	pS.setString(1, productID);
+	pS.executeUpdate();
+	ResultSet rS = pS.executeQuery();
+	
+	rS.close();
+	pS.close();
+	connection.close();
+	
+	return rS;		
+}
+
+
+
+
 
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,6 +1,7 @@
 module retake {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.sql;
 
 	opens application to javafx.graphics, javafx.fxml;
 }
